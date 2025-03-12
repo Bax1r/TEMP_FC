f1029d460b9f2e43f4d049b6b49f0839e1e9c1b0
Bax1r <143042840+Bax1r@users.noreply.github.com>
2025-03-11 18:31:29 -0700

    Add files via upload

---
+++ Simple.py
@@ -0,0 +1,109 @@
+"""
+File Name: Simple.py
+Author: Bryan Insfran
+Description: Class meant to simplify the use
+of sqlite3 by creating methods for that run
+the sqlite commands in a neater manner
+
+Warning: Must pass all parameters as strings
+Example: self.insert('table_name', 'values')
+"""
+
+class Simplify():
+    def __init__(self):
+        pass
+
+    # Creates a table with the given name
+    # Can take any number of varible arguments
+    def create_table(self, name, *vars):
+        initial = ('''CREATE TABLE ''' + name + ''' (''')
+        
+        temp = 0
+
+        for var in vars:
+            if temp == 0:
+                initial += var
+                temp += 1
+                continue
+            initial += (''', ''')
+            initial += var
+
+        initial += ''');'''
+
+        return initial
+
+    # Deletes given table if it already exists
+    def remove_table(self, table):
+        return('''DROP TABLE IF EXISTS ''' + table)
+
+    # Similar to create_table inserts into given table
+    # Can take any number of values (should be the amount of "columns" in a table)
+    def insert(self, table, *values):
+        formatted_values = []
+
+        for value in values:
+            if isinstance(value, str):
+                formatted_values.append(f"'{value}'")  # Wrap strings in single quotes
+            else:
+                formatted_values.append(str(value))  # Convert non-strings to strings
+
+        values_str = ", ".join(formatted_values)
+
+        return ('''INSERT INTO ''' + table + ''' VALUES (''' + values_str + ''')''')
+
+    # Returns all values in given table if no condition is given, condition is optional
+    # Condition arugment runs on comparison operators (=, >=, <=, !=, LIKE, NOT, etc.)
+    def search_all(self, table, condition=None):
+        if condition == None:
+            return ('''SELECT * FROM ''' + table)
+        return ('''SELECT * FROM ''' + table + ''' WHERE ''' + condition)
+
+    def search_vars(self, table, *vars):
+        initial = '''SELECT '''
+        
+        temp = 0
+
+        for var in vars:
+            if temp == 0:
+                initial += var
+                temp += 1
+                continue
+            initial += (''',''')
+            initial += var
+
+        initial += ''' FROM '''
+        initial += table
+
+        return initial
+    
+    # Ordered by ASC or DESC
+    # Order_var correspondes to a column
+    def ordered_search(self, table, order_var, order='ASC', *vars):
+        found = self.search_vars(table, *vars)
+        
+        found += ''' ORDER BY '''
+        found += (order_var + ''' ''')
+        found += order
+
+        return found
+
+    # Deletes any entry that matches given condition
+    def remove(self, table, condition=None):
+        if condition == None:
+            return ('''DELETE from ''' + table)
+        return ('''DELETE from ''' + table + ''' WHERE ''' + condition)
+    
+    def update_vars(self, table, condition=None, *vars):
+        temp = 0
+
+        for var in vars:
+            if temp == 0:
+                vari = var
+                temp += 1
+                continue
+            vari += (''', ''')
+            vari += var
+
+        if condition == None:
+            return ('''UPDATE ''' + table + ''' SET ''' + vari)
+        return ('''UPDATE ''' + table + ''' SET ''' + vari + ''' WHERE ''' + condition)
---
+++ server.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, redirect, request
+from flask import url_for
+#import sqlite3
+import sqlitecloud
+from Simple import Simplify
+
+simple = Simplify()
+test_conn = sqlitecloud.connect("sqlitecloud://ccd05tfthz.g1.sqlite.cloud:8860/Testing?apikey=Mji9QZnn0DLv8by9woBTc105GxkTltAVbcixpOF71Cg")
+test_cursor = test_conn.cursor()
+
+test_cursor.execute(simple.remove_table('testing'))
+test_cursor.execute(simple.create_table('testing', 'Firstname TEXT', 'Lastname TEXT', 'Email TEXT', 'Age INTEGER', 'Role TEXT', 'Activity TEXT', 'Ethnicity_Race TEXT', 'Comments TEXT'))
+
+test_conn.commit()
+test_conn.close()
+
+app = Flask(__name__)
+
+@app.route("/")
+def start():
+	return redirect(url_for('home'))
+
+@app.route("/home")
+def home():
+	return render_template('testing.html')
+
+@app.route("/survey", methods = ['POST', 'GET'],)
+def survey():
+	if request.method == 'GET':
+		return render_template('test_survey.html')
+	elif request.method == 'POST':
+		test_conn = sqlitecloud.connect("sqlitecloud://ccd05tfthz.g1.sqlite.cloud:8860/Testing?apikey=Mji9QZnn0DLv8by9woBTc105GxkTltAVbcixpOF71Cg")
+		test_cursor = test_conn.cursor()
+
+		f_name = request.form['firstname']
+		l_name = request.form['lastname']
+		email = request.form['email']
+		age = request.form['age']
+		role = request.form['role']
+		recommend = request.form['recommend']
+		race = request.form['race']
+		comments = request.form['comment']
+		
+		test_cursor.execute(simple.insert('testing', f_name, l_name, email, age, role, recommend, race, comments))
+
+		test_conn.commit()
+		
+		test_conn.close()
+		
+		return redirect(url_for('home'))
+
+@app.route('/Power_Map')
+def power_map(): 
+    pass
+
+@app.route('/login')
+def login():
+	pass	
+
+if __name__ == "__main__":
+	app.run(debug=True)
