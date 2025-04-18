1dd22754654edb0dd5ba3aeb7ed3f4f1deb277a9
Bax1r <bryaninsfranu3u@gmail.com>
2025-04-18 10:52:57 -0700

    Update server.py
    Power map

--- server.py
+++ server.py
@@ -152,24 +152,100 @@ def self_assess():
 @app.route('/power_map', methods = ['POST', 'GET'],)
 def power_map(): 
 	if request.method == 'GET':
-		return redirect(url_for('home'))
+		#return redirect(url_for('home'))
 		test_conn = sqlitecloud.connect("sqlitecloud://ccd05tfthz.g1.sqlite.cloud:8860/Testing?apikey=Mji9QZnn0DLv8by9woBTc105GxkTltAVbcixpOF71Cg")
 		#test_conn = sqlite3.connect('FLC_database.db')
 		test_cursor = test_conn.cursor()
 
-		data = test_cursor.execute(simple.multiples("plottable", "AGE"))
+		def getColumn(table, data, cursor):
+			# Arguments: table (A table of the database), data (a column), cursor (new cursor for every use of this function)
+			# Returns: List of possible entries, amount of each entry found
+			cursor.execute(simple.multiples(table,data))
+			column = cursor.fetchall()
 		
-		age_data=[]
-		age_data_quantity=[]		
-		for row in data:
-			age_data.append(row[0])
-			age_data_quantity.append(row[-1])
+			column_data=[]
+			column_data_quantity=[]		
+			for row in column:
+				column_data.append(row[0])
+				column_data_quantity.append(row[-1])
 
+			return column_data, column_data_quantity
+
+		##############################################################################################################
+		# General info: Recommend
+		##############################################################################################################
+		cursor_recommend = test_conn.cursor()
+		recommend_data, recommend_data_quantity = getColumn("General_info", "RECOMMEND", cursor_recommend)
+
+		##############################################################################################################
+		# General info: Roles
+		##############################################################################################################
+		cursor_role = test_conn.cursor()
+		role_data, role_data_quantity = getColumn("General_info", "ROLE", cursor_role)
+
+		##############################################################################################################
+		# General info: Gender
+		##############################################################################################################
+		cursor_gender = test_conn.cursor()
+		gender_data, gender_data_quantity = getColumn("General_info", "GENDER", cursor_gender)
+
+		##############################################################################################################
+		# General info: Pronouns
+		##############################################################################################################
+		cursor_pronouns = test_conn.cursor()
+		pronouns_data, pronouns_data_quantity = getColumn("General_info", "PRONOUNS", cursor_pronouns)
+
+		##############################################################################################################
+		#General info: Programs
+		##############################################################################################################
+		cursor_UFSA_CJL = test_conn.cursor()
+		UFSA_CJL_labels, UFSA_CJL_data_quantity = getColumn("General_info", "UFSA_CJL", cursor_UFSA_CJL)
+		cursor_UFSA_LC = test_conn.cursor()
+		UFSA_LC_labels, UFSA_LC_data_quantity = getColumn("General_info", "UFSA_LC", cursor_UFSA_LC)
+		cursor_LA_LC = test_conn.cursor()
+		LA_LC_labels, LA_LC_data_quantity = getColumn("General_info", "LA_LC", cursor_LA_LC)
+		cursor_RUDSDALE_FV = test_conn.cursor()
+		RUDSDALE_FV_labels, RUDSDALE_FV_data_quantity = getColumn("General_info", "RUDSDALE_FV", cursor_RUDSDALE_FV)
+		cursor_LA_GSI = test_conn.cursor()
+		LA_GSI_labels, LA_GSI_data_quantity = getColumn("General_info", "LA_GSI", cursor_LA_GSI)
+		cursor_LA_AC = test_conn.cursor()
+		LA_AC_labels, LA_AC_data_quantity = getColumn("General_info", "LA_AC", cursor_LA_AC)
+		cursor_CALI_SPSP = test_conn.cursor()
+		CALI_SPSP_labels, CALI_SPSP_data_quantity = getColumn("General_info", "CALI_SPSP", cursor_CALI_SPSP)
+		cursor_SPSF = test_conn.cursor()
+		CALI_SPSF_labels, CALI_SPSF_data_quantity = getColumn("General_info", "CALI_SPSF", cursor_SPSF)
+		cursor_CALI_DSC = test_conn.cursor()
+		CALI_DSC_labels, CALI_DSC_data_quantity = getColumn("General_info", "CALI_DSC", cursor_CALI_DSC)
+		cursor_CALI_PCGC = test_conn.cursor()
+		CALI_PCGC_labels, CALI_PCGC_data_quantity = getColumn("General_info", "CALI_PCGC", cursor_CALI_PCGC)
+
+		# The data we want from Programs
+		programs = [[], []]
+		simple.yesCheck(programs, UFSA_CJL_labels, UFSA_CJL_data_quantity, "UFSA_CJL")
+		simple.yesCheck(programs, UFSA_LC_labels, UFSA_LC_data_quantity, "UFSA_LC")
+		simple.yesCheck(programs, LA_LC_labels, LA_LC_data_quantity, "LA_LC")
+		simple.yesCheck(programs, RUDSDALE_FV_labels, RUDSDALE_FV_data_quantity, "RUDSDALE_FV")
+		simple.yesCheck(programs, LA_GSI_labels, LA_GSI_data_quantity, "LA_GSI")
+		simple.yesCheck(programs, LA_AC_labels, LA_AC_data_quantity, "LA_AC")
+		simple.yesCheck(programs, CALI_SPSP_labels, CALI_SPSP_data_quantity, "CALI_SPSP")
+		simple.yesCheck(programs, CALI_SPSF_labels, CALI_SPSF_data_quantity, "CALI_SPSF")
+		simple.yesCheck(programs, CALI_DSC_labels, CALI_DSC_data_quantity, "CALI_DSC_SPSF")
+		simple.yesCheck(programs, CALI_PCGC_labels, CALI_PCGC_data_quantity, "CALI_PCGC_SPSF")
+
+		##############################################################################################################
+		
 		test_conn.commit()
-
 		test_conn.close()
 
-		return render_template('surveyplot.html',age_data=age_data, age_data_quantity=age_data_quantity)
+		return render_template(
+			'surveyplot.html',
+
+			recommend_data=recommend_data, recommend_data_quantity=recommend_data_quantity,
+			role_data=role_data, role_data_quantity=role_data_quantity,
+			gender_data=gender_data, gender_data_quantity=gender_data_quantity,
+			pronouns_data=pronouns_data, pronouns_data_quantity=pronouns_data_quantity,
+			programs_data=programs[0], programs_data_quantity=programs[1]
+			)
 
 @app.route('/login')
 def login():
